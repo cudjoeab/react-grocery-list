7e38dce72a33dcd656079240e9fce44602e1f619
cudjoeab <cudjoeab@gmail.com>
2019-08-29 15:30:36 -0400

    adding initial items in a list, making them a state and rendering them in the list

--- grocery-list/src/List.js
+++ grocery-list/src/List.js
@@ -1,16 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Item from './Item'; 
 
-const List = () => {
 
-    return (
-        <ul id="shoppingList" className="shoppinglist">
-            <Item category={"meat"} item={"steaks"} />
-            <Item category={"produce"} item={"apples"} />
-            <Item category={"dairy"} item={"milk (1L, 3.25%"} />
-            <Item category={"bakery"} item={"boule"} /> 
-        </ul>
-    );
+class List extends Component{
+
+    initialItems = [
+        {
+            name: "steaks",
+            category: "meat"
+        },
+        {
+            name: "apples",
+            category: "produce"
+        },
+        {
+            name: "milk (1L, 3.25%)",
+            category: "dairy"
+        },
+        {
+            name: "baguette",
+            category: "bakery"
+        },
+    ]
+
+    state = {
+        items : this.initialItems
+    }
+
+    render() {
+
+        const listItems = this.state.items.map(
+            (item, index) => <Item key={index} { ...item} />
+        )
+        console.log(listItems)
+        return (
+            <ul id="shoppingList" className="shoppinglist">
+            { listItems }
+            </ul>
+        );
+
+    }
+    
 };
 
 export default List; 
