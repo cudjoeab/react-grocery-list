3b63952894310a0d84109d3b6efba855e1845a74
cudjoeab <cudjoeab@gmail.com>
2019-08-28 23:41:53 -0400

    created single item component

--- grocery-list/src/Item.js
+++ grocery-list/src/Item.js
@@ -2,18 +2,13 @@ import React from 'react';
 
 const Item = () => {
     return (
-        <li className="meat">
-          <button>-</button>
+        <div className="meat">
+          <button type="button" className="btn down">-</button>
           <span>3 Steaks</span>
-          <button>+</button>
-        </li>
-    //   <div className="channel">
-    //     <button type="button" className="btn up" onClick={ () => { updateValue(value+1) } }>+</button>
-    //     <input type="text" className="txt" value={value} onChange={ (event) => { updateValue(event.target.value) } } />
-    //     <button type="button" className="btn down" onClick={ () => { updateValue(value-1) } }>-</button>
-    //   </div>
-    )
-  }
+          <button type ="button" className="btn up">+</button>
+        </div>
+    );
+  };
   
   export default Item;
   
