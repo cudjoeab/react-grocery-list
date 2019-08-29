6236d15d20cebbd2e50ca181910f95a56a8476ec
cudjoeab <cudjoeab@gmail.com>
2019-08-28 23:38:04 -0400

    decomposed app.js to import List

--- grocery-list/src/App.js
+++ grocery-list/src/App.js
@@ -1,33 +1,14 @@
 // Homepage 
 
 import React from 'react';
-// import List from './List'; 
+import List from './List'; 
 
 const App = () => {
   return (
-    <ul id="shoppingList" className="shoppinglist">
-        <li className="meat">
-          <button>-</button>
-          <span>3 Steaks</span>
-          <button>+</button>
-        </li>
-        <li className="prod">
-          <button>-</button>
-          <span>4 Apples</span>
-          <button>+</button>
-        </li>
-        <li className="dairy">
-          <button>-</button>
-          <span>1 Milk (1L, 2%)</span>
-          <button>+</button>
-        </li>
-        <li className="bakery">
-          <button>-</button>
-          <span>2 Baguettes</span>
-          <button>+</button>
-        </li>
-      </ul>
+    
+    <List/>
+    
   );
-}
+};
 
 export default App;
