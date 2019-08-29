c254fb8586ee39dc7e23f6d9604692520a441086
cudjoeab <cudjoeab@gmail.com>
2019-08-28 23:45:42 -0400

    created the List component

---
+++ grocery-list/src/List.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Item from './Item'; 
+
+const List = () => {
+
+    return (
+        <ul id="shoppingList" className="shoppinglist">
+            <Item />
+            <Item />
+            <Item />
+            <Item /> 
+        </ul>
+    );
+};
+
+export default List; 
